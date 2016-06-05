c07bb570a308f48a1106a8477a601d6db14ef661
sunsehoon <parkmi0128@naver.com>
2016-06-05 22:39:31 +0900

    end.c
    hahaha

---
+++ end.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+	char a;
+	char b;
+	char c;
+	while (1){
+		printf("문자 입력 (end 입력시 프로그램 종료) : ");
+		scanf("%c", &a);
+		scanf("%c", &b);
+		scanf("%c", &c);
+
+		getchar();
+		if (a == 'e')
+		{
+			if (b=='n')
+				if (c=='d')
+		
+			exit(1);
+		}
+		else
+			;
+
+	}
+	return 0;
+}
+
